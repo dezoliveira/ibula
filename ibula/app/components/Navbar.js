3b8f10882664c276c252a5e836342d453f4b0e8f
Andres Oliveira <andresoliveira@protonmail.com>
2024-04-17 12:37:19 -0300

    GET Medicines e estrutura dos Cards

--- ibula/app/components/Navbar.js
+++ ibula/app/components/Navbar.js
@@ -9,7 +9,7 @@ import Link from 'next/link';
 export default function TopNavbar() {
   return (
     <>
-      <Navbar bg="success" data-bs-theme="dark">
+      <Navbar bg="success" data-bs-theme="dark" fixed={true}>
         <Container>
           <Navbar.Brand>
             <Link href="/" className='link'>
--- ibula/app/page.js
+++ ibula/app/page.js
@@ -1,7 +1,55 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { Container, Card, Button, Col, Row } from "react-bootstrap";
+
 export default function Home() {
+  
+  const [medicines, setMedicines] = useState([])
+
+  useEffect(() => {
+    getMedicines()
+  }, [])
+
+  const getMedicines = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/data', {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      
+      const data = await response.json()
+      setMedicines(data)
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }  
+
   return (
     <main>
-      <h1>home</h1>
+      <Container className="p-4">
+        <Row>
+          {medicines && medicines.map((medicine) => (
+            <Col lg={6} md={6} sm={12}>
+              <Card id={medicine.id} className="m-4 d-flex flex-column justify-content-between gap-4" style={{ width: '35rem', minHeight: '300px' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>{medicine.name}</Card.Title>
+                  <Card.Text>
+                    {medicine.published_at}
+                  </Card.Text>
+                  <Card.Text>
+                    {medicine.company}
+                  </Card.Text>
+                  <Button variant="danger">Baixar Bula</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </main>
   );
 }
