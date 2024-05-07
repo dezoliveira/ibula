0ea8ccc0082f1aaf18ad0f6047dfc5cc9f4d20c8
Andrés Oliveira <85899665+dezoliveira@users.noreply.github.com>
2024-05-07 03:05:27 +0000

    environment test

--- app/hooks/loadMedicine.js
+++ app/hooks/loadMedicine.js
@@ -1,9 +1,12 @@
+const apiUrl = process.env.NEXT_PUBLIC_API
+
 export async function loadMedicine(id) {
 
   // await new Promise(resolve => setTimeout(resolve, 3000))
+  // const response = await fetch(`http://localhost:3000/data/${id}`
 
   try {
-    const response = await fetch(`http://localhost:3000/data/${id}`, {
+    const response = await fetch(`${apiUrl}/${id}`, {
       method: 'GET',
       
       headers: {
--- app/hooks/loadMedicines.js
+++ app/hooks/loadMedicines.js
@@ -1,9 +1,11 @@
+const apiUrl = process.env.NEXT_PUBLIC_API
+
 export async function loadMedicines() {
   
   // await new Promise(resolve => setTimeout(resolve, 3000))
 
   try {
-    const response = await fetch('http://localhost:3000/data', {
+    const response = await fetch(apiUrl, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
