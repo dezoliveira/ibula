2e0a404e30c6af3e047b0b3735a30054b5ff5410
Andrés Oliveira <85899665+dezoliveira@users.noreply.github.com>
2024-05-08 02:14:52 +0000

    .env.local added

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app"
 import { getDatabase } from 'firebase/database'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCNd4sVGMYE1YL74qqqiXZ-0o5CO4-tWDY",
-  authDomain: "ibula-2d5f3.firebaseapp.com",
-  databaseURL: "https://ibula-2d5f3-default-rtdb.firebaseio.com",
-  projectId: "ibula-2d5f3",
-  storageBucket: "ibula-2d5f3.appspot.com",
-  messagingSenderId: "79130430132",
-  appId: "1:79130430132:web:e17ce34b4f01e3dceb65e1"
-};
+const firebaseConfig = { 
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
